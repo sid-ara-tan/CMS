c329f0b4dfaf0b2017e1028579c6dcdc73bbf80f
unknown <User@SNIPPER-PC.(none)>
2013-03-16 01:22:12 +0600

    footer edited

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/CMS/';
+$config['base_url']	= 'http://localhost/course/';
 
 /*
 |--------------------------------------------------------------------------
